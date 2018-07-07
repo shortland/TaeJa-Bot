f0e7cb0f652aa1a2776f616ef1f43c3a8c6abe9a
shortland <ilankleiman@gmail.com>
2018-07-07 13:49:05 -0400

    Requests using Mojo::Useragent instead of native curl

--- Component/MessageRequest.pm
+++ Component/MessageRequest.pm
@@ -3,91 +3,103 @@ package Component::MessageRequest;
 use v5.10;
 use JSON;
 use Try::Tiny;
+use YAML::Tiny;
+use MIME::Base64;
 use Time::HiRes;
-# Custom Below
-use ApiKeys;
 
 use Exporter;
 our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(MakeDiscordGet MakeDiscordPostJson); # can be
+our @EXPORT = qw(MakeDiscordGet MakeDiscordPostJson); # defaults to
+my $yaml = YAML::Tiny->read('config.yml');
+my $API_DISCORD = $yaml->[0]->{discord_api};
 
-#can be
-our @EXPORT_OK = qw(MakeDiscordGet MakeDiscordPostJson);
-#default
-our @EXPORT = qw(MakeDiscordGet MakeDiscordPostJson);
-
+# @_[0] = endpoint /users/\@me
+# @_[1] = 
+# @_[2] = return decoded json, 0=no, 1=yes
+# @_[3] = sleep time, if !defined then default.2
 sub MakeDiscordGet {
-	my @parms = @_;
-	#parms[0] = endpoint /users/\@me
-	#parms[1] = 
-	#parms[2] = return decoded json, 0=no, 1=yes
-	#parms[3] = sleep time, if !defined then default.2
-	my $userAgent = "DiscordBot (http://ilankleiman.com, 4.0.0)";
-	my $contentType = "Content-Type: application/x-www-form-urlencoded";
-	my $authorizeCode = "Authorization: Bot $API_DISCORD";
-	my $baseURL = "https://discordapp.com/api";
-	my $response = `curl -s --max-time 5 -A "$userAgent" -H "$contentType" -H "$authorizeCode" "${baseURL}@{parms[0]}" -L`;
-	#say $response;
+	my ($endpoint, $etc, $return_json, $sleep_time) = @_;
+    my $useragent = 'DiscordBot (http://ilankleiman.com, 4.0.0)';
+    my $content_type = 'application/x-www-form-urlencoded';
+	my $api_key = 'Bot ' . $API_DISCORD;
+	my $base_url = 'https://discordapp.com/api';
+    my $ua = Mojo::UserAgent->new;
+	my %headers = (
+		'Authorization'		=> $api_key,
+		'Content-Type' 		=> $content_type,
+		'User-Agent' 		=> $useragent
+	);
+	my $response = $ua->get($base_url . $endpoint => \%headers)->result->body;
 	try {
-		$response = decode_json($response) if (@parms[2] =~ /^1$/);
+		$response = decode_json($response) if ($return_json =~ /1/);
 	}
 	catch {
-		$response = "";
+		$response = '';
 	};
-	if(!defined @parms[3] || @parms[3] eq "") {
-		sleep(.2);
+	if (!defined $sleep_time || $sleep_time eq '') {
+		Time::HiRes::sleep(0.250);
 	}
 	else {
-		sleep(@parms[3]);
+		Time::HiRes::sleep($sleep_time);
 	}
 	return $response;
 }
 
+# @_[0] = endpoint /users/\@me
+# @_[1] = json to post
+# @_[2] = return decoded json, 0=no, 1=yes
+# @_[3] = post type, -X POST/PUT/DELETE/etc...
+# @_[4] = sleep time, if !defined then default 1
+# @_[5] = base64 or no
 sub MakeDiscordPostJson {
 	my @parms = @_;
-	#say "I want to post...\n";
-	#parms[0] = endpoint /users/\@me
-	#parms[1] = json to post
-	#parms[2] = return decoded json, 0=no, 1=yes
-	#parms[3] = post type, -X POST/PUT/DELETE/etc...
-	#parms[4] = sleep time, if !defined then default 1
-	#parms[5] = base64 or no
-	my $getType;
-	if ( (@parms[3] =~ /^()$/) || (!defined(@parms[3])) ) {
-		$getType = "POST";
-	}
-	else {
-		$getType = @parms[3];
-	}
+	my ($endpoint, $json, $return_json, $type, $sleep_time, $base64) = @_;
 	my $message;
-	if ( (@parms[5] =~ /^()$/) || (!defined(@parms[5])) ) {
-		$message = $parms[1];
+	if ($base64 eq '' || !defined $base64) {
+		$message = $json;
 	}
-	elsif ( @parms[5] =~ /^base64$/ ) {
-		use MIME::Base64;
-		$message = decode_base64(@parms[1]);
+	elsif ($base64 eq 'base64') {
+		$message = decode_base64($json);
 	}
 	else {
-		$message = $parms[1];
+		$message = $json;
 	}
-	my $userAgent = "DiscordBot (http://ilankleiman.com, 4.0.0)";
-	my $contentType = "Content-Type: application/json";
-	my $authorizeCode = "Authorization: Bot $API_DISCORD";
-	my $baseURL = "https://discordapp.com/api";
-	my $response = `curl -s --max-time 5 -X $getType -d '$message' -A "$userAgent" -H "$contentType" -H "$authorizeCode" "${baseURL}@{parms[0]}" -L`;
+    my $useragent = 'DiscordBot (http://ilankleiman.com, 4.0.0)';
+    my $content_type = 'application/json';
+	my $api_key = 'Bot ' . $API_DISCORD;
+	my $base_url = 'https://discordapp.com/api';
+    my $ua = Mojo::UserAgent->new;
+	my %headers = (
+		'Authorization'		=> $api_key,
+		'Content-Type' 		=> $content_type,
+		'User-Agent' 		=> $useragent
+	);
+	my $response;
+	if ($type eq '' || !defined $type || $type eq "POST") {
+		$response = $ua->post($base_url . $endpoint => \%headers => $message)->result->body;
+	}
+	elsif ($type eq "PUT") {
+		$response = $ua->put($base_url . $endpoint => \%headers => $message)->result->body;
+	}
+	elsif ($type eq "DELETE") {
+		$response = $ua->delete($base_url . $endpoint => \%headers => $message)->result->body;
+	}
+	else {
+		die "Unknown method to request data";
+	}
 	try {
-		$response = decode_json($response) if (@parms[2] =~ /^(1)$/);
+		$response = decode_json($response) if ($return_json =~ /1/);
 	}
 	catch {
-		$response = "";
+		# $response = $response;
 	};
-
-	if(!defined @parms[4] || @parms[4] eq "") {
-		sleep(1);
+	if (!defined $sleep_time || $sleep_time eq '') {
+		Time::HiRes::sleep(0.250);
 	}
 	else {
-		sleep(@parms[4]);
+		Time::HiRes::sleep($sleep_time);
 	}
-
 	return $response;
 }
 
